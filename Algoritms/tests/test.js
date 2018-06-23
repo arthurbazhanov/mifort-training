f834d3829b111f75f5126385ab4c80918e7353bf
arthurbazhanov <arthur.bazhanov@mifort.org>
2018-06-23 16:42:29 +0300

    Added test for function brackets(added should)

--- Algoritms/tests/test.js
+++ Algoritms/tests/test.js
@@ -4,35 +4,35 @@ const brackets = require('.././brackets');
 
 describe('brackets', () => {
 
-    it('return 1',  () => {
+    it('should return 1',  () => {
         assert.equal(brackets('---(++++)----'), 1);
     });
 
-    it('return 1',  () => {
+    it(' should return 1',  () => {
         assert.equal(brackets(''), 1);
     });
 
-    it('return 1',  () => {
+    it(' should return 1',  () => {
         assert.equal(brackets('before (middle []) after'), 1);
     });
 
-    it('return 0',  () => {
+    it('should return 0',  () => {
         assert.equal(brackets(')('), 0);
     });
 
-    it('return 1',  () => {
+    it('should return 1',  () => {
         assert.equal(brackets('}{'), 1);
     });
 
-    it('return 0',  () => {
+    it('should return 0',  () => {
         assert.equal(brackets('<( >)'), 0);
     });
 
-    it('return 1',  () => {
+    it('should return 1',  () => {
         assert.equal(brackets('([<>()]<>)'), 1);
     });
 
-    it('return 0',  () => {
+    it('should return 0',  () => {
         assert.equal(brackets('( [)'), 0);
     });
 
